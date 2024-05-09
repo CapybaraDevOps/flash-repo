762e7efec57c91d4ba3ac8e5f21ddcc569981f8d
Mykhailo <muhaluk322@gmail.com>
2024-05-09 06:28:23 +0300

    Integrate modules for a working prototype

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ import hashlib
 
 ############## Initialization ##############
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb:27017")
 # Mongodb database
 db = client.flask_database
 # Collection
@@ -25,59 +25,92 @@ clients = db.clients
 def index():
     # Get client's IP addr
     client_ip = request.remote_addr
-    # IP Hash
-    ip_hash = hashlib.sha256(client_ip.encode()).hexdigest()
+    ip_hash = get_hash(client_ip)
     # Check if Client data exist
     client_data = ip_hash_exists(ip_hash)
+    
+    if request.method == 'POST':
+        action = request.form.get('action')
+        database_create(client_ip, action, ip_hash)
+        if action == "yes":
+            return redirect(url_for('form'))
+        if action == "no":
+            return redirect(url_for('declined'))
+        if action == "update":
+            return redirect(url_for('form'))
+        if action == "delete":
+            clients.delete_one({"ip_hash": ip_hash})
+            return redirect(url_for('index'))
+    return render_template('index.html', client_data=client_data)
 
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    # Get client's IP addr
+    client_ip = request.remote_addr
+    ip_hash = get_hash(client_ip)
+    # Get client's IP addr
+    client_data = ip_hash_exists(ip_hash)
     if request.method == 'POST':
         data = request.form.get('data')
-        action = request.form.get('action', 'decline')
-
-        if client_data:
-            # If client data exists, update or delete
-            database_update(action, ip_hash, data)
-            return redirect(url_for('index'))
-        else:
-            # Update database
-            database_create(data, action, client_ip, ip_hash)
-            return redirect(url_for('index'))
+        database_update(ip_hash=ip_hash, data=data)
+        return redirect(url_for('index'))
     all_data = clients.find()
-    return render_template('index.html', client_ip=client_ip, clients=all_data, client_data=client_data)
+    return render_template('form.html', client_ip=client_ip, clients=all_data, client_data=client_data)
+
+@app.route("/declined", methods=['GET', 'POST'])
+def declined():
+    # Get client's IP addr
+    client_ip = request.remote_addr
+    ip_hash = get_hash(client_ip)
+    if request.method == 'POST':
+            action = request.form.get('action')
+            if action == "delete":
+                clients.delete_one({"ip_hash": ip_hash})
+                return redirect(url_for('index'))
+            if action == "accept":
+                database_update(ip_hash, action=action, client_ip=client_ip)
+                return redirect(url_for('form'))
+    return render_template('declined.html')
+
+# Get IP Hash
+def get_hash(client_ip):
+    # Hashing IP addr
+    return hashlib.sha256(client_ip.encode()).hexdigest()
 
 # Func to create record in database
-def database_create(data, action, client_ip, ip_hash):
+def database_create(client_ip, action, ip_hash):
     # Get a value for client_id
     client_id = get_next_client_id()
-    if action == 'accept':
+    if action == 'yes':
         clients.insert_one({
             'client_id': client_id,
             'ip': client_ip, 
-            'status': action,
-            'data': data,
+            'status': "accepted",
             'ip_hash': ip_hash 
             })
-        return redirect(url_for('index'))
-    elif action == 'decline':
+    elif action == 'no':
         clients.insert_one({
             'client_id': client_id,
-            'status': action,
-            'ip_hash': ip_hash
+            'status': "declined",
+            'ip_hash': ip_hash 
             })
-        return redirect(url_for('index'))
     
-# Func to update or delete record in database
-def database_update(action, ip_hash, data):
-    if action == "update":
-        update_result = clients.update_one(
+# Func to update database
+def database_update(ip_hash, client_ip=None, data=None, action=None):
+    update_data = {}
+    if action:
+        update_data['status'] = 'accepted'
+    if client_ip:
+        update_data['ip'] = client_ip
+    if data:
+        update_data['data'] = data
+    
+    if update_data:
+        clients.update_one(
             {'ip_hash': ip_hash},
-            {'$set': {'data': data}}
+            {'$set': update_data},
+            upsert=False  # Set to True if you want to create a new document when no document matches the query
         )
-        if update_result.modified_count > 0:
-            print("Client data updated successfully.")
-        return redirect(url_for('index'))
-    elif action == "delete":
-        clients.delete_one({"ip_hash": ip_hash})
 
 def ip_hash_exists(ip_hash):
     # Check if the client data already exist and return it
@@ -102,5 +135,5 @@ def get_next_client_id():
 
 # Run web-server
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5050)
 
